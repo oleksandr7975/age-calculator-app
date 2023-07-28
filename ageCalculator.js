64bffd5cbb8b0e68ae4f6083a09e68e24e8a6165
oleksandr babich <83249103+oleksandr7975@users.noreply.github.com>
2023-07-28 21:31:44 +0300

    Add files via upload

--- ageCalculator.js
+++ ageCalculator.js
@@ -2,11 +2,17 @@ function myTest(){
     let birthDay = document.querySelector('.day').value;
     let birthMonth = document.querySelector('.month').value;
     let birthYear = document.querySelector('.year').value;
+    const dateNow = new Date().getDate();
+    const yearNow = new Date().getFullYear();
+    const monthNow = new Date().getMonth();
+    let resultDate = dateNow-birthDay;
+    let resultYear = yearNow-birthYear;
+    let resultMonth = (monthNow+1)-birthMonth;
 
     console.log(day);
-    document.querySelector('.result-years').innerHTML = birthDay;
-    document.querySelector('.result-months').innerHTML = birthMonth;
-    document.querySelector('.result-days').innerHTML = birthYear;
+    document.querySelector('.result-years').innerHTML = resultYear;
+    document.querySelector('.result-months').innerHTML = resultMonth;
+    document.querySelector('.result-days').innerHTML = resultDate;
     
 
 }
